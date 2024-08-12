071f037e81f925b55afb0cc9ee04f756168982ea
Bart van Erp <44952318+bartvanerp@users.noreply.github.com>
2024-08-12 13:27:02 +0200

    improve stability poisson

--- src/distributions/poisson.jl
+++ src/distributions/poisson.jl
@@ -1,5 +1,6 @@
 export Poisson
 
+using SpecialFunctions: logfactorial
 import Statistics: mean, var
 
 struct Poisson{T}
@@ -9,6 +10,14 @@ end
 mean(p::Poisson) = p.λ
 var(p::Poisson) = p.λ
 
-pdf(p::Poisson, x::Int) = x >= 0 ? exp(-p.λ) * p.λ^x / factorial(x) : 0
+function logpdf(p::Poisson, x::Int)
+    if x < 0
+        return -Inf
+    else
+        # return -p.λ + x * log(p.λ) - log(factorial(x))
+        return -p.λ + x * log(p.λ) - logfactorial(x)
+    end
+end
+pdf(p::Poisson, x::Int) = exp(logpdf(p, x))
 
 KL_loss(p::Poisson, q::Poisson) = p.λ * (log(p.λ) - log(q.λ)) + q.λ - p.λ
--- test/distributions/poisson_tests.jl
+++ test/distributions/poisson_tests.jl
@@ -1,6 +1,6 @@
 @testitem "Poisson distribution" begin
 
-    using UnboundedBNN: Poisson, mean, var, pdf, KL_loss
+    using UnboundedBNN: Poisson, mean, var, pdf, logpdf, KL_loss
 
     @test mean(Poisson(1.0)) ≈ 1.0
     @test mean(Poisson(2.0)) ≈ 2.0
@@ -20,6 +20,7 @@
     @test pdf(Poisson(2.0), 3) ≈ exp(-2.0) * 8.0 / 6.0
     @test pdf(Poisson(2.0), -1) ≈ 0
     @test pdf(Poisson(2.0), -2) ≈ 0
+    @test pdf(Poisson(3.0), 500) ≈ 0.0
 
     @test KL_loss(Poisson(1.0), Poisson(1.0)) ≈ 0.0
     @test KL_loss(Poisson(1.0), Poisson(2.0)) ≈ - log(2) + 1
