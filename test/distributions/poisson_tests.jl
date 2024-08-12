dba02fecd11f49aab45e5df8d022fbea62d6f84a
Bart van Erp <44952318+bartvanerp@users.noreply.github.com>
2024-08-12 08:46:46 +0200

    add tests poisson

---
+++ test/distributions/poisson_tests.jl
@@ -0,0 +1,29 @@
+@testitem "Poisson distribution" begin
+
+    using UnboundedBNN: Poisson, mean, var, pdf, KL_loss
+
+    @test mean(Poisson(1.0)) ≈ 1.0
+    @test mean(Poisson(2.0)) ≈ 2.0
+    @test var(Poisson(1.0)) ≈ 1.0
+    @test var(Poisson(2.0)) ≈ 2.0
+
+    @test pdf(Poisson(1.0), 0) ≈ exp(-1.0)
+    @test pdf(Poisson(1.0), 1) ≈ exp(-1.0)
+    @test pdf(Poisson(1.0), 2) ≈ exp(-1.0) / 2.0
+    @test pdf(Poisson(1.0), 3) ≈ exp(-1.0) / 6.0
+    @test pdf(Poisson(1.0), -1) ≈ 0
+    @test pdf(Poisson(1.0), -2) ≈ 0
+
+    @test pdf(Poisson(2.0), 0) ≈ exp(-2.0)
+    @test pdf(Poisson(2.0), 1) ≈ exp(-2.0) * 2.0
+    @test pdf(Poisson(2.0), 2) ≈ exp(-2.0) * 4.0 / 2.0
+    @test pdf(Poisson(2.0), 3) ≈ exp(-2.0) * 8.0 / 6.0
+    @test pdf(Poisson(2.0), -1) ≈ 0
+    @test pdf(Poisson(2.0), -2) ≈ 0
+
+    @test KL_loss(Poisson(1.0), Poisson(1.0)) ≈ 0.0
+    @test KL_loss(Poisson(1.0), Poisson(2.0)) ≈ - log(2) + 1
+    @test KL_loss(Poisson(2.0), Poisson(1.0)) ≈ 2 * log(2) - 1
+    @test KL_loss(Poisson(2.0), Poisson(2.0)) ≈ 0.0
+
+end
