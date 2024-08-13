6decd825d264123bfbe693c5eb2722a292ef0c72
Bart van Erp <44952318+bartvanerp@users.noreply.github.com>
2024-08-13 13:09:47 +0200

    add single output network

--- src/UnboundedBNN.jl
+++ src/UnboundedBNN.jl
@@ -2,7 +2,7 @@ module UnboundedBNN
 
 using StatsFuns, Functors, LogExpFunctions
 
-export Unbounded
+export Unbounded, UnboundedSingle
 
 include("math.jl")
 
@@ -11,7 +11,9 @@ include("losses/KL.jl")
 include("layers/chain.jl")
 include("layers/skip.jl")
 include("layers/linear.jl")
+include("layers/linear_bbb.jl")
 include("layers/relu.jl")
+include("layers/softmax.jl")
 
 include("distributions/poisson.jl")
 include("distributions/truncated_poisson.jl")
@@ -25,10 +27,20 @@ struct Unbounded{T1, T2, T3, P, TP}
 end
 @functor Unbounded (input_layer, hidden_layers, output_layers, posterior) # exclude prior from training
 
+struct UnboundedSingle{T1, T2, T3, P, TP}
+    input_layer::T1
+    hidden_layers::T2
+    output_layers::T3
+    prior::P
+    posterior::TP
+end
+@functor UnboundedSingle (input_layer, hidden_layers, output_layers, posterior) # exclude prior from training
+
 
 (m::Unbounded)(x_mean, x_var) = _applymodel(m, m.posterior, x_mean, x_var)
+(m::UnboundedSingle)(x) = _applymodel(m, m.posterior, x)
 
-@generated function _applymodel(model, post::SafeTruncatedPoisson{MIN, MAX, T}, x_mean, x_var) where {MIN, MAX, T}
+@generated function _applymodel(model::Unbounded, post::SafeTruncatedPoisson{MIN, MAX, T}, x_mean, x_var) where {MIN, MAX, T}
 
     # create symbols
     state_symbols = vcat(:x_mean, :x_var, [gensym() for _ in 1:(2*MAX + 4)])
@@ -47,5 +59,22 @@ end
 
 convert_to_tuples(x) = ntuple(i -> :(($(x[2*i-1]), $(x[2*i]))), div(length(x),2))
 join_as_tuples(x) = :( tuple($(x...) ))
+
+@generated function _applymodel(model::UnboundedSingle, post::SafeTruncatedPoisson{MIN, MAX, T}, x) where {MIN, MAX, T}
+
+    # create symbols
+    state_symbols = vcat(:x, [gensym() for _ in 1:(MAX + 2)])
+    output_symbols = [gensym() for _ in 1:(MAX - MIN + 1)]
+
+    # create calls
+    expand_call = :( $(state_symbols[2]) = model.input_layer($(state_symbols[1])) )
+    state_calls = [:( $(state_symbols[i+2]) = model.hidden_layers[$i]($(state_symbols[i+1])) ) for i in 1:MAX+1]
+    output_calls = [:( $(output_symbols[i]) = model.output_layers[$(MIN+i)]($(state_symbols[MIN + i + 1])) ) for i in 1:(MAX - MIN + 1)]
+    return_call = :( return $(join_as_tuples(output_symbols) ) )
+    calls = vcat(expand_call, state_calls, output_calls, return_call)
+
+    return Expr(:block, calls...)
+
+end
 
 end # module UnboundedBNN
--- src/layers/chain.jl
+++ src/layers/chain.jl
@@ -8,10 +8,16 @@ end
 Chain(xs...) = Chain(xs)
 
 (c::Chain)(x_mean, x_var) = _applychain(c.layers, x_mean, x_var)
+(c::Chain)(x) = _applychain(c.layers, x)
 
 @generated function _applychain(layers::Tuple{Vararg{Any,N}}, x_mean, x_var) where {N}
     symbols = vcat(:x_mean, :x_var, [gensym() for _ in 1:2*N])
     calls = [:( ($(symbols[2*i+1]), $(symbols[2*i+2])) = layers[$i]($(symbols[2*i-1]), $(symbols[2*i]))) for i in 1:N]
+    return Expr(:block, calls...)
+end
+@generated function _applychain(layers::Tuple{Vararg{Any,N}}, x) where {N}
+    symbols = vcat(:x, [gensym() for _ in 1:N])
+    calls = [:( $(symbols[i+1]) = layers[$i]($(symbols[i])) ) for i in 1:N]
     return Expr(:block, calls...)
 end
 
