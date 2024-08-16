203c0a5a5540206831dbdd99755c5157b4163a71
Bart van Erp <44952318+bartvanerp@users.noreply.github.com>
2024-08-16 18:55:11 +0200

    add Heaviside function

--- src/math.jl
+++ src/math.jl
@@ -1,3 +1,14 @@
+export H, Heaviside
+export softplus, invsoftplus, sigmoid
+
 const softplus = log1pexp
 const invsoftplus = logexpm1
 const sigmoid = logistic
+
+struct ShiftedHeaviside{T}
+    a::T
+end
+
+Heaviside() = return ShiftedHeaviside(0)
+
+const H = Heaviside
