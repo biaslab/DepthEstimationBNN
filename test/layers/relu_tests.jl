e39262034fd0a1f85675ef8817dc93b3d849ba9b
Bart van Erp <44952318+bartvanerp@users.noreply.github.com>
2024-08-19 12:01:19 +0200

    add tests relu

---
+++ test/layers/relu_tests.jl
@@ -0,0 +1,29 @@
+@testitem "ReLU layer" begin
+    
+    using UnboundedBNN: relu
+
+    @testset "mean-var" begin
+        
+        m = randn(1000)
+        v = rand(1000)
+        m_out, v_out = relu(m, v)
+        out = relu.(m, v)
+        @test all(m_out .== first.(out))
+        @test all(v_out .== last.(out))
+        @test all(m_out .>= m)
+        @test all(m_out .>= 0)
+        @test all(v_out .<= v)
+        @test all(v_out .>= 0)
+
+    end
+
+    @testset "mean" begin
+        
+        m = randn(1000)
+        out = relu(m)
+        @test all(out .>= m)
+        @test all(out .>= 0)
+
+    end
+
+end
