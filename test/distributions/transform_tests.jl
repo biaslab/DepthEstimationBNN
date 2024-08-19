b1850d99072cff463572c90fdd1c7d26a1ee7637
Bart van Erp <44952318+bartvanerp@users.noreply.github.com>
2024-08-19 10:08:19 +0200

    add tests transform

---
+++ test/distributions/transform_tests.jl
@@ -0,0 +1,17 @@
+@testitem "Transformed Distribution" begin
+    
+    using UnboundedBNN: TransformedDistribution, transform, truncate, discretize, TruncatedDistribution, DiscreteDistribution
+
+    d1 = TransformedDistribution(Normal(0, 1), x -> truncate(x, 0, 1))
+    @test transform(d1) == TruncatedDistribution(Normal(0, 1), 0, 1)
+
+    d2 = TransformedDistribution(
+        Normal(0, 1),
+        (
+            x -> truncate(x, 0, 1),
+            x -> discretize(x)
+        )
+    )
+    @test transform(d2) == DiscreteDistribution(TruncatedDistribution(Normal(0, 1), 0, 1))
+
+end
