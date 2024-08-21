14bfefc94c9db34bafab4756451a9c505d685ff8
Bart van Erp <44952318+bartvanerp@users.noreply.github.com>
2024-08-21 11:11:41 +0200

    fix type stability transform

--- src/distributions/transform.jl
+++ src/distributions/transform.jl
@@ -6,6 +6,17 @@ struct TransformedDistribution{D, F}
 end
 @functor TransformedDistribution (dist, )
 
-transform(d::TransformedDistribution) = reduce(∘, Iterators.reverse(d.f))(d.dist)
+transform(d::TransformedDistribution) = _transform(d)
 transform(d::TransformedDistribution{D,<:Function}) where { D } = d.f(d.dist)
 transform(d::Distribution) = d
+
+@generated function _transform(d::TransformedDistribution{D,<:NTuple{N, Any}}) where {D,N}
+
+    # create symbols
+    symbols = vcat(:(d.dist), [gensym() for _ in 1:N])
+
+    # create calls
+    calls = [:( $(symbols[i+1]) = d.f[$i]($(symbols[i])) ) for i in 1:N]
+
+    return Expr(:block, calls...)
+end
