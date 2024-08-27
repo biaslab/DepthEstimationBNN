ef7decf94dbeef4de23557450fad7f857b2e8f6c
Bart van Erp <44952318+bartvanerp@users.noreply.github.com>
2024-08-27 11:39:53 +0200

    fix bug softmax

--- src/layers/softmax.jl
+++ src/layers/softmax.jl
@@ -2,13 +2,13 @@ export Softmax
 
 using StatsFuns: softmax
 
-struct Softmax{N} 
+struct Softmax{N}
     Softmax(N::Int) =  new{N}()
 end
 
 function (l::Softmax)(x_mean, x_var; rng=default_rng())
-    tmp = randn(rng, 2) .* sqrt.(x_var) .+ x_mean
-    return tmp .- logsumexp(tmp), nothing
+    tmp = randn(rng, size(x_var)) .* sqrt.(x_var) .+ x_mean
+    return tmp .- logsumexp(tmp, dims=1), nothing
 end
 
 (l::Softmax)(x::Vector) = x .- logsumexp(x)
