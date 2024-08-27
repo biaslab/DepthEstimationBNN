e46950c13ba88341ffbbe3c1f17fac7444304f7d
Bart van Erp <44952318+bartvanerp@users.noreply.github.com>
2024-08-27 11:24:03 +0200

    add tests relu

--- test/layers/relu_tests.jl
+++ test/layers/relu_tests.jl
@@ -17,10 +17,36 @@
 
     end
 
+    @testset "ReLU mean-var" begin
+        
+        layer = ReLU()
+        m = randn(1000)
+        v = rand(1000)
+        m_out, v_out = layer(m, v)
+        out = layer.(m, v)
+        @test all(m_out .== first.(out))
+        @test all(v_out .== last.(out))
+        @test all(m_out .>= m .- 1e-10)
+        @test all(m_out .>= 0)
+        @test all(v_out .<= v .+ 1e-10)
+        @test all(v_out .>= 0)
+
+    end
+
     @testset "mean" begin
         
         m = randn(1000)
         out = relu(m)
+        @test all(out .>= m)
+        @test all(out .>= 0)
+
+    end
+
+    @testset "ReLU mean" begin
+        
+        layer = ReLU()
+        m = randn(1000)
+        out = layer(m)
         @test all(out .>= m)
         @test all(out .>= 0)
 
